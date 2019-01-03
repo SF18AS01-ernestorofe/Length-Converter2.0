0dce98b6c23753521ce2a0fe397fadca69165656
Ernesto Rodrigues <ernesto.ro92@gmail.com>
2019-01-03 10:42:43 -0500

    Initial Commit

---
+++ Length Converter2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Length Converter2.0
+//
+//  Created by Ernesto Rodrigues on 1/3/19.
+//  Copyright © 2019 Ernesto Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Length Converter2.0Tests/Length_Converter2_0Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Length_Converter2_0Tests.swift
+//  Length Converter2.0Tests
+//
+//  Created by Ernesto Rodrigues on 1/3/19.
+//  Copyright © 2019 Ernesto Rodrigues. All rights reserved.
+//
+
+import XCTest
+@testable import Length_Converter2_0
+
+class Length_Converter2_0Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
